1f6cb7554d7f1b0e19c6d1bc7fbace7170671c72
Eric Xu <e.xulander@gmail.com>
2013-07-25 15:43:40 -0700

    add signup, login, and logout functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Lolly01::Application.routes.draw do
-  resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :urls
+  resources :users, only: [:index, :new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: "urls#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130723003851) do
+ActiveRecord::Schema.define(version: 20130725205139) do
 
   create_table "urls", force: true do |t|
     t.string   "original_url"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20130723003851) do
     t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
