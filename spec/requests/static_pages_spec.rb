0dc1e2dc89cf8b1652034e7611f560f15d360e86
Eric Xu <e.xulander@gmail.com>
2013-08-29 00:58:31 -0400

    add more tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,20 @@ describe "Static pages" do
 	subject { page }
 	let(:base_title) { "LOL.ly" }
 
+	it "should have the right links on the home page layout" do
+		visit root_path
+		click_link "What's under the hood?"
+  	expect(page).to have_title(full_title('About'))
+  	click_link "Log In"
+  	expect(page).to have_title(full_title('Log In'))
+  	click_link "Smile and sign up"
+  	expect(page).to have_title(full_title('Sign Up'))
+	end
+
   describe "About page" do
   	before { visit about_path }
 
   	it { should have_title(full_title('About')) }
-  	it { should have_content('Techniques Used') }
+  	it { should have_selector('h1', text: 'Techniques Used') }
   end
 end
